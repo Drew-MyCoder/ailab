192f3ceb6368076d4fbc02d06961b4cb9af2bbbc
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-07-05 11:45:38 +0000

    zod bug

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,22 +1,9 @@
 "use server";
 
-import { toast } from "sonner";
-import { z } from "zod";
-
 
 const googleScriptURL = process.env.NEXT_PUBLIC_GOOGLE_SCRIPT as string;
 
 const subscriptionScriptURL = process.env.NEXT_PUBLIC_SUBSCRIPTION as string;
-
- const schema = z.object({
-          email: z.string().email({ message: "Invalid email address" }),
-      });
-
-const schemas = z.object({
-        firstName: z.string().min(3, { message: "Firstname must be at least 3 characters" }),
-        lastName: z.string().min(3, { message: "Lastname must be at least 3 characters" }),
-        email: z.string().email({ message: "Invalid email address" }),
-    });
 
 
 export const addRegistration = async (formData: FormData) => {
@@ -24,14 +11,6 @@ export const addRegistration = async (formData: FormData) => {
         const firstName = formData.get("firstName")?.toString() || "";
         const lastName = formData.get("lastName")?.toString() || "";
         const email = formData.get("email")?.toString() || "";
-
-        const validation = schemas.safeParse({ firstName, lastName, email });
-
-        if (!validation.success) {
-            const errorMsg = validation.error.errors[0]?.message ?? "Invalid input";
-            toast.error(errorMsg);
-            return;
-        }
 
 
     try {
@@ -63,14 +42,6 @@ export const addRegistration = async (formData: FormData) => {
 export const addSubscription = async (subscriptionData: FormData) => {
         
         const email = subscriptionData.get("email")?.toString() || "";
-
-        const validation = schema.safeParse({ email });
-
-        if (!validation.success) {
-            const errorMsg = validation.error.errors[0]?.message ?? "Invalid input";
-            toast.error(errorMsg);
-            return;
-        }
 
     try {
         const res = await fetch(subscriptionScriptURL, {
