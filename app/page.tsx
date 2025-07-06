d2011d66fa89aaf11f1988c3881c3c13c07931cf
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-07-06 04:34:39 +0000

    ...bug for lanscape leftalign

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,16 @@ import { PricingSection } from "@/components/pricing";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <HeroSection />
-      <FeaturesSection />
-      <HowItWorksSection />
-      <BenefitsSection />
-      <PricingSection />
-      <CTASection />
+    <div className="flex flex-col min-h-screen items-center">
+      <div className="w-full max-w-7xl px-4">
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <BenefitsSection />
+        <PricingSection />
+        <CTASection />
+      </div>
     </div>
-  )
+  );
 }
+
