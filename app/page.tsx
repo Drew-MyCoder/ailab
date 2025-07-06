22ec4367aba869c038acc193b6f60cea80f066cf
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-07-06 04:09:15 +0000

    ....

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { BenefitsSection } from "@/components/benefits";
 import { CTASection } from "@/components/cta-section";
-import { FeaturesSection } from "@/components/Features";
-import { HeroSection } from "@/components/HeroSection";
+// import { FeaturesSection } from "@/components/Features";
+// import { HeroSection } from "@/components/HeroSection";
 // import { HowItWorksSection } from "@/components/How-It-works";
 import { PricingSection } from "@/components/pricing";
 
@@ -9,8 +9,8 @@ import { PricingSection } from "@/components/pricing";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen align-middle">
-      <HeroSection />
-      <FeaturesSection />
+      {/* <HeroSection /> */}
+      {/* <FeaturesSection /> */}
       {/* <HowItWorksSection /> */}
       <BenefitsSection />
       <PricingSection />
